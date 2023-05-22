ae2d2884b88ffad698697ce9c0c1dc50dfe9e52b
xxz <xxz911@rambler.ru>
2023-05-22 09:40:38 +0300

    Настроил приветствие и сообщение о помощи, тестовый эхо ответ

--- base.py
+++ base.py
@@ -1,3 +1,49 @@
 import logging
-
 from aiogram import Bot, Dispatcher, executor, types
+
+# Указываем токен
+API_TOKEN = ''
+
+# Включаем логирование
+logging.basicConfig(level=logging.DEBUG)
+
+# Инициализируем бота и dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+# Вывод помощи
+@dp.message_handler(commands=['help'])
+async def send_help(message: types.Message):
+    await message.reply(
+        text='''
+Мои команды:
+/help -- увидеть это сообщение
+''',
+        reply=False)
+
+
+# Вывод приветствия
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply(
+        text='''
+Привет!     
+Я бот, который поможет тебе контролировать и анализировать расходы на авто.
+
+Пример ввода расходов: 2500 бенз
+''',
+        reply=False)
+    await send_help(message=message)
+
+
+# Пока что эхо ответ
+@dp.message_handler(content_types=types.ContentType.TEXT)
+async def do_echo(message: types.Message):
+    text = message.text
+    if text and isinstance(text, str) and not text.startswith('/'):
+        await message.reply(text=text)
+
+# опрашивает сервер
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
